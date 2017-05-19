b6a767d37f7d96eb22e5d43843ee27770a12cb24
Roberto Perez Cubero <robj.perez@gmail.com>
2017-05-19 12:01:09 +0200

    Updating sample

--- app/src/main/java/com/tokbox/multipartytest/MainActivity.kt
+++ app/src/main/java/com/tokbox/multipartytest/MainActivity.kt
@@ -3,10 +3,9 @@ package com.tokbox.multipartytest
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import kotlinx.android.synthetic.activity_main.*;
-import org.jetbrains.anko.async
+import kotlinx.android.synthetic.main.activity_main.*;
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import org.json.JSONObject
 import java.net.URL
@@ -18,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         button_join_room.setOnClickListener(object: View.OnClickListener{
             override fun onClick(v: View?) {
-                async() {
+                doAsync {
                     val sessData = getSessionData(input_room_name.text.toString())
                     uiThread {
                         val act = Intent(this@MainActivity, RoomActivity::class.java)
@@ -31,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getSessionData(roomName: String): SessionData {
-        val jsonText = URL("https://meet.tokbox.com/$roomName").readText()
+        val jsonText = URL("https://opentokdemo.tokbox.com/room/$roomName/info").readText()
         val jsonObj = JSONObject(jsonText)
 
         return SessionData(jsonObj.getString("sessionId"),
--- app/src/main/java/com/tokbox/multipartytest/RoomActivity.kt
+++ app/src/main/java/com/tokbox/multipartytest/RoomActivity.kt
@@ -1,26 +1,30 @@
 package com.tokbox.multipartytest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.RelativeLayout
 import com.opentok.android.*
-import kotlinx.android.synthetic.activity_room.*
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.activity_room.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 import org.jetbrains.anko.toast
-import java.util.*
 
 private class SubscriberData (val containerView: RelativeLayout) {
-    public var otSubscriber: Subscriber? = null
+    var otSubscriber: Subscriber? = null
 }
 
-class RoomActivity : AppCompatActivity(), Session.SessionListener {
+class RoomActivity : AppCompatActivity(), Session.SessionListener, AnkoLogger {
     private val TAG = RoomActivity::class.java.name
+    private val PERMISSION_REQUEST_CODE = 123
 
     private var otSession : Session?= null
     private var otPub : Publisher? = null
-    private var subContainerArray : Array<SubscriberData>? = null
+    private var subContainerArray : Array<SubscriberData> = emptyArray()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,36 +37,66 @@ class RoomActivity : AppCompatActivity(), Session.SessionListener {
                 .or(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
                 .or(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
 
-        OpenTokConfig.setAPIRootURL("https://anvil-tbdev.opentok.com", false);
         subContainerArray = arrayOf(
                 SubscriberData(sub_container_1),
                 SubscriberData(sub_container_2),
                 SubscriberData(sub_container_3))
 
-        val sd = (intent.extras.get("sessionData") as SessionData)!!
-        otSession = Session(this, sd.apiKey, sd.sessionId)
-        otSession!!.setSessionListener(this)
-        otSession!!.connect(sd.token)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(
+                            Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO
+                    ),
+                    PERMISSION_REQUEST_CODE
+            )
+        } else {
+            createAndConnectSession()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_REQUEST_CODE
+                && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            createAndConnectSession()
+        } else {
+            toast("App does not have permission to record video and audio. Exiting")
+            finish()
+        }
+    }
+
+    private fun createAndConnectSession() {
+        val sd = (intent.extras.get("sessionData") as SessionData)
+        otSession = Session.Builder(this, sd.apiKey, sd.sessionId).sessionOptions(object: Session.SessionOptions() {
+            override fun useTextureViews(): Boolean {
+                return true
+            }
+        }).build()
+        otSession?.setSessionListener(this)
+        otSession?.connect(sd.token)
+
     }
 
     private fun findEmptySubscriberIndex() : Int? {
-        var idx = 0
-        for (s in subContainerArray!!) {
-            if(s.otSubscriber == null) return idx
-            idx++
+        subContainerArray.forEachIndexed { index, subscriberData ->
+            if (subscriberData.otSubscriber == null) return index
         }
         return null
     }
 
-    private fun findSubscriber(stream: Stream) : Int? {
-        var idx = 0
-        for (s in subContainerArray!!) {
-            if (s.otSubscriber!!.stream.streamId == stream.streamId) return idx
-            idx++
+    private fun findSubscriber(stream: Stream?) : Int? {
+        stream?.let {
+            val sub = subContainerArray.find {
+                it.otSubscriber?.stream?.streamId == stream.streamId
+            }
+            val idx = subContainerArray.indexOf(sub)
+            if (idx != -1) {
+                return idx
+            }
         }
         return null
     }
-
 
     private fun addVideoView(container: RelativeLayout, view: View) {
         val lp = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
@@ -75,43 +109,46 @@ class RoomActivity : AppCompatActivity(), Session.SessionListener {
     }
 
     override fun onError(sess: Session?, error: OpentokError?) {
-
+        error("Session connection error $error")
     }
 
     override fun onConnected(sess: Session?) {
-        Log.d(TAG, "Session Connected")
-        otPub = Publisher(this)
-        otPub!!.publishAudio = false
-        otPub!!.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,
-                BaseVideoRenderer.STYLE_VIDEO_FILL)
-
-        otSession!!.publish(otPub!!)
-        addVideoView(publisher_container, otPub!!.view)
+        debug("Session Connected")
+        otPub = Publisher.Builder(this).build()
+        otPub?.let {
+            it.publishAudio = false
+            it.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,
+                    BaseVideoRenderer.STYLE_VIDEO_FILL)
+            otSession?.publish(it)
+            addVideoView(publisher_container, it.view)
+        }
     }
 
     override fun onStreamReceived(sess: Session?, stream: Stream?) {
         val idx = findEmptySubscriberIndex()
-        if (idx != null) {
-            val sub = Subscriber(this, stream)
+        idx?.let {
+            val sub = Subscriber.Builder(this, stream).build()
             sub.subscribeToAudio = false
             sub.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,
                     BaseVideoRenderer.STYLE_VIDEO_FILL)
-            otSession!!.subscribe(sub)
+            otSession?.subscribe(sub)
 
-            subContainerArray!![idx].otSubscriber = sub
-            addVideoView(subContainerArray!![idx].containerView, sub.view)
-        } else {
-            toast("Ignoring next peer, this sample only supports 3 subscribers")
+            subContainerArray[it].otSubscriber = sub
+            addVideoView(subContainerArray[it].containerView, sub.view)
+            return
         }
+
+        toast("Ignoring next peer, this sample only supports 3 subscribers")
     }
 
     override fun onStreamDropped(sess: Session?, stream: Stream?) {
-        val idx = findSubscriber(stream!!)
-        if (idx != null) {
-            val sd = subContainerArray!![idx]
-            otSession!!.unsubscribe(sd.otSubscriber)
-            removeVideoView(sd.containerView, sd.otSubscriber!!.view)
-
+        val idx = findSubscriber(stream)
+        idx?.let {
+            val sd = subContainerArray[it]
+            sd.otSubscriber?.let {
+                otSession?.unsubscribe(it)
+                removeVideoView(sd.containerView, it.view)
+            }
             sd.otSubscriber = null
         }
     }
